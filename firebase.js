c31f2d6e5911a8bcd794932c53c50c18197f975d
carlosdeuganda <carlosdeuganda@gmail.com>
2026-09-24 03:24:15 +0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,19 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDcAa3lAITasot_HHODfNhdnrQbqNQVgUE",
+  authDomain: "fir-academy-d950d.firebaseapp.com",
+  projectId: "fir-academy-d950d",
+  storageBucket: "fir-academy-d950d.firebasestorage.app",
+  messagingSenderId: "705122530570",
+  appId: "1:705122530570:web:7d1649b142b10ca32fb6f3",
+  measurementId: "G-LXER8DHEFE"
+};
+
+// Initialize the app once
+export const app = initializeApp(firebaseConfig);
+
+// Initialize Auth once and share it with the whole project
+export const auth = getAuth(app);
